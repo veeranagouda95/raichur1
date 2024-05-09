0738210638bad262187c5cf807d62364b514bad9
HP <HP@DESKTOP-4IM3C73>
2024-05-09 14:08:54 +0530

    This program i will add the two numbers

--- raichur/src/raichurbandan/entryraichur.java
+++ raichur/src/raichurbandan/entryraichur.java
@@ -1,7 +1,21 @@
 package raichurbandan;
 
 public class entryraichur {
-public static void main(String[] args) {
+	/***
+	 * This method jvm will be searching in program
+	 * @hi
+	 * @param args
+	 */
+public static void main(String[] args) 
+{
 	System.out.println("i am coming to raichur okey");
+	add(5, 6);
+}
+
+public static int add(int a,int b) 
+{
+	 int res=a+b;
+	System.out.println(res);
+	return res; 
 }
 }
